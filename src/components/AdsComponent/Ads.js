ec2135a59ebd2c3ce95c80ea3e688ea702184b22
Skorin Vladislav <43670004+Skorin-Vladislav@users.noreply.github.com>
2022-12-18 14:18:18 +0300

    added promo banner

---
+++ src/components/AdsComponent/Ads.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Ads.css";
+function Ads() {
+  return (
+    <div className="ads-wrapper">
+      <p>
+        <strong>Try it free 7 days</strong> then $20/mo. thereafter
+      </p>
+    </div>
+  );
+}
+
+export default Ads;
